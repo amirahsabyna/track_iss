666b67d4c436567595be0001f75bd208e34149fd
amirahsabyna <82787961+amirahsabyna@users.noreply.github.com>
2021-12-16 10:36:00 +0800

    Add files via upload

---
+++ trackiss/Readme.txt
@@ -0,0 +1,11 @@
+Instruction:
+
+1. Install: 
+    1) python
+    2) pip install turtle
+    3) pip install urllib3
+    4) pip install times
+    5) pip install geocoder
+    6) pip install geopy
+
+2. Run code and then the result will appear on terminal
---
+++ trackiss/main.py
@@ -0,0 +1,61 @@
+#Real time ISS tracker
+
+import json, turtle, urllib.request, time, geocoder, webbrowser
+from geopy.geocoders import Nominatim
+
+
+#A first JSON request to retrieve the name of all the astronauts currently in space.
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print("There are currently " + str(result["number"]) + " astronauts in space:")
+print("")
+
+people = result["people"]
+
+for p in people:
+  print(p["name"] + " on board of " + p["craft"])
+
+#Display information on world map using Python Turtle
+screen = turtle.Screen()
+screen.setup(720, 360)
+screen.setworldcoordinates(-180, -90, 180, 90)
+#Load the world map picture
+screen.bgpic("world-map.gif")
+  
+screen.register_shape("iss.gif")
+iss = turtle.Turtle()
+iss.shape("iss.gif")
+iss.setheading(45)
+iss.penup()
+
+while True:
+  #A JSON request to retrieve the current longitude and latitude of the IIS space station (real time)  
+  url = "http://api.open-notify.org/iss-now.json"
+  response = urllib.request.urlopen(url)
+  result = json.loads(response.read())
+    
+  #Let's extract the required information
+  location =result["iss_position"]
+  lat = location["latitude"]
+  lon = location["longitude"]
+
+    
+  #Output informationon screen
+  print("\nLatitude: " + str(lat))
+  print("Longitude: " +str(lon))
+  
+  #Plot the ISS on the map  
+  iss.goto(float(lon), float(lat))
+  
+  #refresh position every 5 seconds
+  time.sleep(5)
+  
+  #datetime
+  localtime = time.asctime(time.localtime(time.time()))
+  print ("Local current time:", localtime)
+  
+  #location name
+  geolocator = Nominatim(user_agent = "geoapiExercises")
+  location = geolocator.geocode("iss_position")
+  print("Location Name: ", location)
